87b982db45b3f7ddd3180b92f6a7053b5629198f
Kulakovich Daniil <92942037+DanikKul@users.noreply.github.com>
2023-02-26 22:34:01 +0300

    Updated ExceptionController

--- src/main/java/com/workshop/epam_workshop/controllers/ExceptionController.java
+++ src/main/java/com/workshop/epam_workshop/controllers/ExceptionController.java
@@ -1,8 +1,9 @@
 package com.workshop.epam_workshop.controllers;
 
 import com.workshop.epam_workshop.errors.NegativeException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
@@ -10,13 +11,17 @@ import org.springframework.web.server.ResponseStatusException;
 @ControllerAdvice
 public class ExceptionController {
 
+    private static final Logger logger = LogManager.getLogger(ExceptionController.class);
+
     @ExceptionHandler(value = NegativeException.class)
     public ResponseStatusException negativeException() {
+        logger.error("Error: Negative value");
         return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Negative value error");
     }
 
     @ExceptionHandler(value = NumberFormatException.class)
     public ResponseStatusException parseException() {
+        logger.error("Error: Can't parse value");
         return new ResponseStatusException(HttpStatus.BAD_REQUEST, "Parsing error");
     }
 
