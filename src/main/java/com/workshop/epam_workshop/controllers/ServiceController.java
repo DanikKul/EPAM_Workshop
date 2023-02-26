bd5e0448accf892d73606cafea2a1086d8a98e81
Kulakovich Daniil <92942037+DanikKul@users.noreply.github.com>
2023-02-26 23:28:21 +0300

    Updated ExceptionController

--- src/main/java/com/workshop/epam_workshop/controllers/ServiceController.java
+++ src/main/java/com/workshop/epam_workshop/controllers/ServiceController.java
@@ -2,6 +2,8 @@ package com.workshop.epam_workshop.controllers;
 
 import com.workshop.epam_workshop.model.Converter;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.apache.logging.log4j.LogManager;
@@ -16,11 +18,20 @@ public class ServiceController{
     private static final Logger logger = LogManager.getLogger(ServiceController.class);
 
     @GetMapping(value = "/answer")
-    public ResponseEntity<?> getAnswer (@RequestParam(value="value") String value){
+    public ResponseEntity<?> getAnswer (@RequestParam(value="value", defaultValue = "") String value){
+        if (value.isEmpty())
+            return new ResponseEntity<>("<p style=\"font-size: 40px; text-align:center; height:100%; display: flex; flex-direction: column; justify-content: center;\">Please enter non-negative value</p>", HttpStatus.OK);
         logger.info("Trying to parse data...");
         double parsedValue = Converter.validateData(value.strip());
         logger.info("Parsing completed!");
         return new ResponseEntity<>(new Converter(parsedValue), HttpStatusCode.valueOf(200));
+    }
+
+    @GetMapping(value = "/")
+    public ResponseEntity<?> home (@RequestParam (value = "user", defaultValue = "User") String value) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/answer?value=5");
+        return new ResponseEntity<String>(headers, HttpStatus.FOUND);
     }
 
 }
